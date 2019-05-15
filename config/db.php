c0a5428229f78b85ef095886fa679836f0302d7b
hilmy assodiky <assodikyhilmy@gmail.com>
2019-05-15 14:05:23 +0700

    register dan kontak

--- config/db.php
+++ config/db.php
@@ -2,9 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=pinjaman_online',
-    'username' => 'root',
-    'password' => '',
+    // 'dsn' => 'mysql:host=localhost;dbname=pinjaman_online',
+    // 'username' => 'root',
+    // 'password' => '',
+    'dsn' => 'mysql:host=192.168.1.111;dbname=pinjaman_online',
+    'username' => 'root123',
+    'password' => 'root@123',
     'charset' => 'utf8',
 
  //    'class' => 'yii\db\Connection',
--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -1,17 +1,22 @@
 <?php
 
 namespace app\controllers;
+use yii\filters\auth\QueryParamAuth;
+use app\models\Nasabah;
+use app\models\NasabahBukuTelepon;
+use app\models\Akun;
+use Yii;
 
 class ApiController extends \yii\rest\Controller
 {
-    public function behaviors(){
-        $behaviors = parent::behaviors();
-        $behaviors['authenticator'] = [
-            'class' => QueryParamAuth::className(),
-            'except' => ['login','registrasi']
-        ];
-        return $behaviors;
-    }
+    // public function behaviors(){
+    //     $behaviors = parent::behaviors();
+    //     $behaviors['authenticator'] = [
+    //         'class' => QueryParamAuth::className(),
+    //         'except' => ['login','registrasi']
+    //     ];
+    //     return $behaviors;
+    // }
 
     /**
      * @inheritdoc
@@ -27,179 +32,151 @@ class ApiController extends \yii\rest\Controller
     public function actionRegister(){
 
         $param = \Yii::$app->request->post();
-        $param['photo'] = isset($_FILES['photo']) ? $_FILES['photo'] : null;
+        $param['ktp'] = isset($_FILES['ktp']) ? $_FILES['ktp'] : "";
+        $param['with_ktp'] = isset($_FILES['with_ktp']) ? $_FILES['with_ktp'] : "";
 
         // var_dump($param);die;
 
-        $errorMsg = array();
+        $response = array();
 
-        if($param['password'] != "" && $param['name'] != "" && $param['sex'] != "" && $param['email'] != "" && $param['phone'] != "" && $param['department'] != "" && $param['expertise'] != "" && $param['company'] != ""){
+        if($param['ktp'] != "" && $param['with_ktp'] != "" && $param['sex'] != "" && $param['email'] != "" && $param['phone_number'] != "" && $param['name'] != "" && $param['password'] != "" && $param['address'] != "" && $param['birth_place'] != "" && $param['birth_date'] != ""){
 
-            $user = User::find()->where(['email' => $param['email']])->one();
+            $nasabah = Nasabah::find()->where(['email' => $param['email']])->one();
 
-            if($user == null){
-                $user = new User();
-                $user_profile = new UserProfile();
+            if($nasabah == null){
+            	$transaction = Yii::$app->db->beginTransaction();
+                try{
+	                $akun = new Akun();
+	                $akun_id = $akun->createAkun($akun, $param['password'], 2);
 
-                $name = $this->split_name($param['name']);
+	                if($akun_id){
 
-                $user->generateAuthKey();
-                $user->setPassword($param['password']);
-                $user->email = $param['email'];
-                $user->created_at = Yii::$app->formatter->asTimestamp(date('Y-d-m h:i:s'));
-                $user->updated_at = Yii::$app->formatter->asTimestamp(date('Y-d-m h:i:s'));
-                $user->role = 5;
-                $user->status = 0;
+	                	$nasabah = new Nasabah();
+	                	$nasabah->id_akun = $akun_id;
+	                	$nasabah->nama = $param['name'];
+	                	$nasabah->alamat = $param['address'];
+	                	$nasabah->tempat_lahir = $param['birth_place'];
+	                	$nasabah->tanggal_lahir = $param['birth_date'];
+	                	$nasabah->jenis_kelamin = $param['sex'];
+	                	$nasabah->email = $param['email'];
+	                	$nasabah->nomor_telepon = $param['phone_number'];
 
-                if($user->save()){
-                    $user_profile->gender = $param['sex'];
-                    $user_profile->phone = $param['phone'];
-                    $user_profile->first_name = $name[0];
-                    $user_profile->last_name = $name[1];
-                    $user_profile->id_departement = $param['department'];
-                    $user_profile->id_jobtitle = $param['expertise'];
-                    $user_profile->id_user = $user->id;
-                    $user_profile->id_perusahaan = $param['company'];
+	                    if($nasabah->save(false)){
 
-                    if(isset($param['photo'])){
+	                    	$extension = $this->getFileExtension($param['ktp']['name']);
+	                        $name = "ktp-".$nasabah->id."-".time(). '.' . $extension;
+	                        $filedest = 'foto/' . $name;
+	                        move_uploaded_file($param['ktp']['tmp_name'], $filedest);
+	                        $nasabah->foto_ktp = $name;
 
-                        $extension = $this->getFileExtension($param['photo']['name']);
-                        $name = "photo-".$user->id."-".time(). '.' . $extension;
-                        $filedest = Yii::getAlias('@frontend/web/user/') . $name;
-                        move_uploaded_file($param['photo']['tmp_name'], $filedest);
-                        $user_profile->photo = $name;
-                    }
+	                        $extension = $this->getFileExtension($param['with_ktp']['name']);
+	                        $name = "with_ktp-".$nasabah->id."-".time(). '.' . $extension;
+	                        $filedest = 'foto/' . $name;
+	                        move_uploaded_file($param['with_ktp']['tmp_name'], $filedest);
+	                        $nasabah->foto_bersama_ktp = $name;
 
-                    if($user_profile->save(false)){
-                        $email = \Yii::$app->mailer->compose('index', ['auth_key'=>$user->auth_key,'status'=>0])
-                                ->setTo($user->email)
-                                ->setFrom(['elearning.lanius@gmail.com' => 'Elearning'])
-                                ->setSubject('Signup Confirmation')
-                                ->send();
-                        $msg['message'] = "Register success! check your e-mail for activate your account";
-                        $msg['status'] = 1;
-                    }else{
-                        $msg['message'] = "Register failed.";
-                        $msg['status'] = 0;
-                    }
+	                        $nasabah->save(false);
 
-                }else{
-                    $msg['message'] = "Register failed.";
-                    $msg['status'] = 0;
+	                        $response['message'] = "Registrasi berhasil";
+	                        $response['status'] = 1;
+	                        $response['customer_id'] = $nasabah->id;
+	                        $transaction->commit();
+	                    }
+	                }
+
+                } catch (\Exception $e) {
+                    $transaction->rollBack();
+                    $response['message'] = $e->getMessage();
+                    $response['status'] = 0;
                 }
-                return $msg;
             }else{
-                $errorMsg['message'] = 'Email sudah digunakan.';
-                $errorMsg['status'] = 0;
-                return $errorMsg;
+                $response['message'] = 'Email sudah digunakan';
+                $response['status'] = 0;
             }
         }else{
-            $errorMsg['message'] = 'Data Tidak boleh kosong.';
-            $errorMsg['status'] = 0;
-            return $errorMsg;
+            $response['message'] = 'Data Tidak boleh kosong';
+            $response['status'] = 0;
         }
+
+        return $response;
     }
 
     public function actionLogin()
     {
         $param=\Yii::$app->request->post();
-        
+
+        $response= array();
         if ($param['email']!='' && $param['password']!='') {
-            $data= array();
-            $user = User::findByEmailAPI($param['email']);
-            if ($user) {
-                if ($user->account) {
-                    if ($user->validatePassword($param['password'])) {
-                        $user->last_login=date('Y-m-d H:i:s');
-                        $user->save(false);
-                        $account['login'] = Account::find()->where(['id'=>$user->account->id])->asArray()->all();
-                        if ($account['login'][0]['logo']) {
-                            $path = \Yii::getAlias('@backend').'/web/upload/account/'.$account['login'][0]['logo'];
-                            if (file_exists($path)) {
-                                $type = pathinfo($path, PATHINFO_EXTENSION);
-                                $data = file_get_contents($path);
-                                if (file_get_contents($path)) {
-                                    switch ($type) {
-                                        case 'jpg':
-                                        case '.jpeg':
-	                                        $source_image = @imagecreatefromjpeg($path);
-	                                        break;
-                                        case 'gif':
-                                            $source_image = @imagecreatefromgif($path);
-                                            break;
-                                        case 'png':
-                                            $source_image = @imagecreatefrompng($path);
-                                            break;
-                                        case 'bmp':
-                                            $source_image = @imagecreatefrombmp($path);
-                                            break;
-                                        default:
-                                            $source_image = false;
-                                            break;
-                                    }
-
-                                    if ($source_image) {
-                                        $source_imagex = imagesx($source_image);
-                                        $source_imagey = imagesy($source_image);
-                                        $dest_imagex = 100;
-                                        $dest_imagey = 100;
-                                        $dest_image = imagecreatetruecolor($dest_imagex, $dest_imagey);
-                                        imagecopyresampled($dest_image, $source_image, 0, 0, 0, 0, $dest_imagex, $dest_imagey, $source_imagex, $source_imagey);
-                                        // header("Content-Type: image/jpeg");
-                                        // echo ;
-
-                                        ob_start (); 
-
-                                          imagejpeg($dest_image,NULL,80);
-                                          $image_data = ob_get_contents(); 
-
-                                        ob_end_clean ();
-
-                                        $base64 = base64_encode($image_data);
-
-                                    }else{
-                                        $base64 = "";
-                                    }                                    
-                                }else{
-                                    $base64 = "";
-                                }
-                            }else{
-                                $base64 = "";
-                            }
-                        }else{
-                            $base64 = "";
-                        }
-
-                        $account['login'][0]['base']=$base64;
-                        if ($user->account->exp_date>date('Y-m-d')) {
-                            $account['login'][0]['free']=0;
-                        }else{
-                            $account['login'][0]['free']=1;
-                        }
-
-                        $account['login'][0]['logo'] =$this->getNameFile().'/account/'.$account['login'][0]['logo'];
-                        $account['login'][0]['account_id'] = $user->account->id;
-                        $account['login'][0]['user_id'] = $user->id;
-                        $account['login'][0]['nama_kategori'] = $account['login'][0]['id_categori_account'] ? CategoriAccount::findOne($account['login'][0]['id_categori_account'])->name : "";
-                        $account['login'][0]['access_token'] = $user->auth_key;
-                        $account['login'][0]['status_valid'] = 1;
-                        return $account;
+            
+            $nasabah = Nasabah::findByEmailAPI($param['email']);
+            if ($nasabah) {
+                if ($nasabah->akun) {
+                    if ($nasabah->validatePassword($param['password'])) {
+                        $response['message'] = 'Berhasil login';
+            			$response['status'] = 1;
                     }else{
-                        $data['login'][0]['status_valid']=0;
-                        return $data;
+                        $response['message'] = 'Password tidak sesuai';
+            			$response['status'] = 0;
                     }
                 }else{
-                    $data['login'][0]['status_valid']=0;
-                    return $data;
+                    $response['message'] = 'Akun tidak ditemukan';
+            		$response['status'] = 0;
                 }
             }else{
-                $data['login'][0]['status_valid']=0;
-                return $data;
+                $response['message'] = 'Nasabah tidak ditemukan';
+            	$response['status'] = 0;
             }
         }else{
-            $data['login'][0]['status_valid']=0;
-            return $data;
+            $response['message'] = 'Data Tidak boleh kosong';
+            $response['status'] = 0;
         }
+
+        return $response;
+    }
+
+    public function actionSendContacts(){
+        $param = Yii::$app->request->post();
+        $response = array();
+
+        $request = json_decode($param['contacts'], true);
+
+        if (isset($request)) {
+        	if ($request['customer_id'] != "") {
+        		$transaction = Yii::$app->db->beginTransaction();
+                try{
+                	foreach ($request['phone_numbers'] as $key => $value) {
+                		$phone_number = new NasabahBukuTelepon();
+                		$phone_number->nama = $value['nama'];
+                		$phone_number->nomor_telepon = $value['nomor_telepon'];
+                		$phone_number->id_nasabah = $request['customer_id'];
+
+                		$phone_number->save(false);
+                	}
+
+                	$transaction->commit();
+                	$response['message'] = 'Berhasil mengirim kontak';
+            		$response['status'] = 1;
+                } catch (\Exception $e) {
+                    $transaction->rollBack();
+                    $response['message'] = $e->getMessage();
+                    $response['status'] = 0;
+                }
+        	} else {
+        		$response['message'] = 'Data Tidak boleh kosong';
+            	$response['status'] = 0;
+        	}
+        } else {
+        	$response['message'] = 'Data Tidak boleh kosong';
+            $response['status'] = 0;
+        }
+
+        return $response; 
+    }
+
+    function getFileExtension($file)
+    {
+        $path_parts = pathinfo($file);
+        return $path_parts['extension'];
     }
 
     public function getNameFile(){
